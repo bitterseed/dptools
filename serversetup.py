83a17bd2825c188558faa008a1798e27bee40ffd
bitterseed <61271102+bitterseed@users.noreply.github.com>
2021-03-06 10:28:44 +1100

    Update serversetup.py

--- serversetup.py
+++ serversetup.py
@@ -1,7 +1,6 @@
 import os
 import random
 import string
-from time import sleep
 
 # clear the screen
 print("\n"*100)
@@ -65,16 +64,13 @@ print(f"We're going to do some system updates in {time} seconds:")
 space(1)
 while time > 0:
         print(time)
-        sleep(1)
         time += -1
 
 os.system("apt update && apt upgrade -y && apt autoremove -y")
-sleep(5)
 
 # add cron job to keep the system up to date
 space(100)
 print("Let's add some cron job to keep the system up to date and reboot the system once a week")
-sleep(5)
 space(1)
 os.system("(crontab -l ; echo '0 0 * * */3 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y')| crontab -")
 os.system("(crontab -l ; echo '0 0 * * */7 sudo reboot')| crontab -")
@@ -83,17 +79,14 @@ os.system("(crontab -l ; echo '0 0 * * */7 sudo reboot')| crontab -")
 space(100)
 print("Let's open some ports and enable the firewall")
 space(1)
-sleep(5)
 os.system("ufw allow ssh/tcp && ufw limit ssh/tcp && ufw allow 19999/tcp && ufw allow 26656/tcp")
 os.system("ufw allow 3000/tcp && ufw allow 3010/tcp & ufw logging on && ufw enable")
-sleep(2)
 
 
 # set up swap space
 space(100)
 print("Let's set up some Swap space")
 space(1)
-sleep(5)
 os.system("sudo swapoff -a")
 os.system("rm /var/swapfile")
 os.system("fallocate -l 4G /var/swapfile")
@@ -102,13 +95,11 @@ os.system("mkswap /var/swapfile")
 os.system("swapon -a")
 if os.system("grep '^/var/swapfile.none.swap.sw.0.0\' /etc/fstab"):
         os.system("echo '/var/swapfile\tnone\tswap\tsw\t0\t0\' >>/etc/fstab")
-sleep(2)
 
 # install fail2ban and enable the service
 space(100)
 print("Let's install 'fail2ban' and enable the service")
 space(1)
-sleep(5)
 os.system("apt install fail2ban -y")
 
 os.system("rm /etc/fail2ban/jail.local")
@@ -117,7 +108,6 @@ my_file.write("[sshd]\nenabled = true\nport = 22\nfilter = sshd\nlogpath = /var/
 
 os.system("systemctl restart fail2ban")
 os.system("systemctl enable fail2ban")
-sleep(2)
 
 # Disable the root login and enable the 'dash' user login
 space(100)
@@ -125,7 +115,6 @@ print("Let's disable the 'root' user login and enable the 'dash' user login")
 print("From now on, you will no longer be able to log in as the 'root' user")
 confirm("Do you understand?")
 space(1)
-sleep(5)
 #open file in read mode
 file = open("/etc/ssh/sshd_config", "r")
 replaced_content = ""
@@ -148,6 +137,4 @@ write_file.close()
 space(10)
 confirm("Step one complete, server is ready for a reboot, do you have your new login details?")
 space(2)
-print("Rebooting in 10 seconds - ctrl-C now if you must")
-sleep(10)
 os.system("sudo reboot")
